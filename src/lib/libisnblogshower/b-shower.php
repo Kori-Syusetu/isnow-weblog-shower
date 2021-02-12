e68ddaf81f8d504920438caa3d379d4f9664e0b5
Kori Syusetu <fumi11224@gmail.com>
2021-02-12 16:24:14 +0000

    ライブラリローダー関連関数に対応、組込み情報形式変更。

--- src/lib/libisnblogshower/b-shower.php
+++ src/lib/libisnblogshower/b-shower.php
@@ -1,7 +1,7 @@
 <?php
 namespace ISnow\LibweBlogShower;
 function lib_init(){
-    $_lib["ISnow"]["LibweBlogShower"] = array(
+    $_lib["byNs"]["ISnow"]["LibweBlogShower"] = array(
         "versioncode" => 1,
         "versionid" => 1.0,
         "versionstr" => "1.0",
@@ -10,4 +10,14 @@ function lib_init(){
             "ISnow\LibDocumentShower" => "1.0<=x<2.0"
         )
     );
+    $_lib["byDir"]["libisnblogshower"] =array(
+        "versioncode" => 1,
+        "versionid" => 1.0,
+        "versionstr" => "1.0",
+        "depends" => array(
+            "Php" => "7.3<=x",
+            "ISnow\LibDocumentShower" => "1.0<=x<2.0"
+        )
+    );
+    lib_logger("blogshower","libisnblogshowerは組み込み関数を正常に終了しました。",ISN_LIB_LOG_INFO,True);
 }
--- src/lib/libisndocshower/d-shower.php
+++ src/lib/libisndocshower/d-shower.php
@@ -1,7 +1,7 @@
 <?php
 namespace ISnow\LibDocumentShower;
 function lib_init(){
-    $_lib["ISnow"]["LibDocumentShower"] = array(
+    $_lib["byNs"]["ISnow"]["LibDocumentShower"] = array(
         "versioncode" => 1,
         "versionid" => 1.0,
         "versionstr" => "1.0",
@@ -9,4 +9,13 @@ function lib_init(){
             "Php" => "7.3<=x"
         )
     );
+    $_lib["byDir"]["libisndocshower"] =array(
+        "versioncode" => 1,
+        "versionid" => 1.0,
+        "versionstr" => "1.0",
+        "depends" => array(
+            "Php" => "7.3<=x"
+        )
+    );
+    lib_logger("docshower","libisndocshowerは組み込み関数を正常に終了しました。",ISN_LIB_LOG_INFO,True);
 }
